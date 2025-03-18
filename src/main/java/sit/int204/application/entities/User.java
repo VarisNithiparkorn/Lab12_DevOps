ea8ff498b9eb77730b56662092bdefe6235d55f8
VARIS NITHIPARKORN <142777819+VarisNithiparkorn@users.noreply.github.com>
2025-03-18 18:31:56 +0700

    Update User.java

--- src/main/java/sit/int204/application/entities/User.java
+++ src/main/java/sit/int204/application/entities/User.java
@@ -4,10 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-
+import jakarta.persistence.Table;
 @Entity
 @Getter
 @Setter
+@Table(name= "users")
 public class User {
     @Id
     private int id;
